28177772677958d390bdcdb56adcff6c5cf3343b
Christian <christiandmelo@gmail.com>
2024-02-04 12:31:34 -0300

    Ajuste para subir corretamente no docker local

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,14 @@ using Domain.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurando LoggerFactory e criando uma instância de ILogger
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+    builder.AddDebug();
+});
+var logger = loggerFactory.CreateLogger<Program>();
+
 // Add services to the container.
 builder.Services.AddControllers();
 
@@ -17,6 +25,8 @@ string secret = "";
 
 if (builder.Environment.IsProduction())
 {
+    logger.LogInformation("Ambiente de Producao detectado.");
+
     builder.Configuration.AddAmazonSecretsManager("us-west-2", "producao-secret");
 
     connectionString = builder.Configuration.GetSection("ConnectionString").Value ?? string.Empty;
@@ -25,7 +35,8 @@ if (builder.Environment.IsProduction())
 }
 else
 {
-    //local
+    logger.LogInformation("Ambiente de Desenvolvimento/Local detectado.");
+
     connectionString = builder.Configuration.GetSection("ConnectionString").Value ?? string.Empty;
 
     secret = builder.Configuration.GetSection("ClientSecret").Value ?? string.Empty;
