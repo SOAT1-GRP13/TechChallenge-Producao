93999e72b124ee85641442fe719716d936b97e3d
Christian <christiandmelo@gmail.com>
2024-02-14 07:48:13 -0300

    Corrigindo erro de quando nao existia pedido no banco

--- Application/Pedidos/Handlers/ConsultarStatusPedidoCommandHandler.cs
+++ Application/Pedidos/Handlers/ConsultarStatusPedidoCommandHandler.cs
@@ -23,25 +23,29 @@ namespace Application.Pedidos.Handlers
 
         public async Task<ConsultarStatusPedidoOutput> Handle(ConsultarStatusPedidoCommand request, CancellationToken cancellationToken)
         {
-            if (request.EhValido())
-            {
-                try
-                {
-                    var pedido = await _pedidoUseCase.ObterPedidoPorId(request.Id);
-
-                    return new ConsultarStatusPedidoOutput(pedido.PedidoStatus, request.Id);
-                }
-                catch (DomainException ex)
-                {
-                    await _mediatorHandler.PublicarNotificacao(new DomainNotification(request.MessageType, ex.Message));
-                }
-            }
-            else
+            if (!request.EhValido())
             {
                 foreach (var error in request.ValidationResult.Errors)
-                {
                     await _mediatorHandler.PublicarNotificacao(new DomainNotification(request.MessageType, error.ErrorMessage));
+
+                return new ConsultarStatusPedidoOutput();
+            }
+
+            try
+            {
+                var pedido = await _pedidoUseCase.ObterPedidoPorId(request.Id);
+
+                if (pedido is null)
+                {
+                    await _mediatorHandler.PublicarNotificacao(new DomainNotification(request.MessageType, "Pedido não encontrado"));
+                    return new ConsultarStatusPedidoOutput();
                 }
+
+                return new ConsultarStatusPedidoOutput(pedido.PedidoStatus, request.Id);
+            }
+            catch (DomainException ex)
+            {
+                await _mediatorHandler.PublicarNotificacao(new DomainNotification(request.MessageType, ex.Message));
             }
             return new ConsultarStatusPedidoOutput();
         }
